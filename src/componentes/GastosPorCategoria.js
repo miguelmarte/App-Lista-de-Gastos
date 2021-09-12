23c021528fe41d44d8ab7736e71cd043b97aea29
Miguel Marte <mjml0412@gmail.com>
2021-09-12 11:41:53 -0400

    agregando obtener gastos por categoria

--- src/componentes/GastosPorCategoria.js
+++ src/componentes/GastosPorCategoria.js
@@ -3,8 +3,19 @@ import { Helmet } from "react-helmet";
 import { Header, Titulo } from "./../elementos/Header";
 import BtnRegresar from "../elementos/BtnRegresar";
 import BarraTotalGastado from "./BarraTotalGastado";
+import useObtenerGastosDelMesPorCategoria from "../hooks/useObtenerGastosDelMesPorCategoria";
+import {
+  ListaDeCategorias,
+  ElementoListaCategorias,
+  Categoria,
+  Valor,
+} from "./../elementos/ElementosDeLista";
+import IconoCategoria from "./../elementos/IconoCategoria";
+import converirAMoneda from "./../funciones/ConvertirAMoneda";
 
 const GastosPorCategoria = () => {
+  const gastosPorCategoria = useObtenerGastosDelMesPorCategoria();
+
   return (
     <>
       <Helmet>
@@ -14,6 +25,19 @@ const GastosPorCategoria = () => {
         <BtnRegresar />
         <Titulo>Gastos por Categoria</Titulo>
       </Header>
+      <ListaDeCategorias>
+        {gastosPorCategoria.map((elemento, index) => {
+          return (
+            <ElementoListaCategorias key={index}>
+              <Categoria>
+                <IconoCategoria id={elemento.categoria} />
+                {elemento.categoria}
+              </Categoria>
+              <Valor>{converirAMoneda(elemento.cantidad)}</Valor>
+            </ElementoListaCategorias>
+          );
+        })}
+      </ListaDeCategorias>
       <BarraTotalGastado />
     </>
   );
